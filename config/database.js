4baeb13bd490603be3638cbb828d4f07d87b5b94
kdaglas <daglach7@gmail.com>
2020-08-30 08:57:30 +0300

    reconnect with server DB

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const { createPool } = require('mysql');
 
 const pool = createPool({
     port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
+    host: process.env.DB_HOST || process.env.HR_HOST,
+    user: process.env.DB_USER || process.env.HR_USER,
+    password: process.env.DB_PASS || process.env.HR_PASS,
+    database: process.env.MYSQL_DB || process.env.HR_MYSQL_DB,
     connectionLimit: 20
 });
 
